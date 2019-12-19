2486f2bb72ad0cc7a5b5deb3ae11c78ff50139b8
Dennis Günnewig <dev@fedux.org>
2019-12-19 13:40:04 +0100

    Fix guard for ligit

--- git_dynamic.go
+++ git_dynamic.go
@@ -5,10 +5,8 @@ package git
 /*
 #include <git2.h>
 #cgo pkg-config: libgit2
-
-#if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 27
-# error "Invalid libgit2 version; this git2go supports libgit2 v0.27"
+#if LIBGIT2_VER_MAJOR != 0 || LIBGIT2_VER_MINOR != 28
+# error "Invalid libgit2 version; this git2go supports libgit2 v0.28"
 #endif
-
 */
 import "C"
